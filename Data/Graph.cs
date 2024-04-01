f2f915c740353a1564199a84c91a0435d4cca066
mirceababtan <91918105+mirceababtan@users.noreply.github.com>
2024-04-01 19:49:31 +0300

    Add project files.

---
+++ Data/Graph.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainForm.Data
+{
+    internal class Graph
+    {
+        public List<Vertex>? Vertices {  get; set; }
+
+        public int[,]? EdgeMap { get; set; }
+
+        public int EdgeCount { get; set; }
+    }
+}
---
+++ Data/Vertex.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainForm.Data
+{
+    internal class Vertex
+    {
+        public required Point Position { get; set; }
+
+        public required double Radius { get; set; }
+
+        public string Notation { get; set; } = "";
+    }
+}
